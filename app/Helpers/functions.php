caa0334b5f824e221ca66fb65bd65cffad34d329
DESKTOP-AEEU8VD\Users <luizrjunior@gmail.com>
2022-12-21 08:10:26 -0300

    Ajuste para adicionar o Helper da aplicação

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,1 +1,19 @@
 <?php
+
+if (!function_exists('retornaValorAntigo')) {
+
+    /**
+     * Retornar Valor Antigo da Variavel
+     * @param $variavel
+     * @param $atributo
+     * @return mixed
+     */
+    function retornaValorAntigo($variavel, $atributo)
+    {
+        if (old($atributo) != "") {
+            $variavel = old($atributo);
+        }
+        return $variavel;
+    }
+}
+
