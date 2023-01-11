5e91a11715fa66edbc69d5cad8ae724782ffce9d
luiz.junior <luiz.junior@mutua.com.br>
2023-01-11 12:52:38 -0300

    Adicionando as funções para carregar Congregações e Categorias de Lançamentos.

--- public/js/general.js
+++ public/js/general.js
@@ -1,3 +1,47 @@
-$(document).ready(function () {
-    $("#itemMenuDashboard").addClass('active');
-});
+function carregarInputCategoriasPsq() {
+    $("#categoria_lancamento_id_psq").html('<option value="">CARREGANDO...</option>');
+    var formURL = top.urlCarregarCategorias + '/carregar';
+    var postData = {
+        _token: $("input[name='_token']").val(),
+        tipo_psq: $("#tipo_psq").val()
+    };
+    $.ajax({
+        type: "POST",
+        url: formURL,
+        data: postData,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            var count = 0;
+            $("#categoria_lancamento_id_psq").html('<option value=""> - - SELECIONE - - </option>');
+            $.each(data, function (index, item) {
+                $("#categoria_lancamento_id_psq").append('<option value="' + index + '">' + item + '</option>');
+                count = 1;
+            });
+        }
+    });
+}
+
+function carregarInputCongregacoesPsq() {
+    $("#congregacao_id_psq").html('<option value="">CARREGANDO...</option>');
+    var formURL = top.urlCarregarCongregacoes + '/carregar';
+    var postData = {
+        _token: $("input[name='_token']").val(),
+        uf_psq: $("#uf_psq").val()
+    };
+    $.ajax({
+        type: "POST",
+        url: formURL,
+        data: postData,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            var count = 0;
+            $("#congregacao_id_psq").html('<option value=""> - - SELECIONE - - </option>');
+            $.each(data, function (index, item) {
+                $("#congregacao_id_psq").append('<option value="' + index + '">' + item + '</option>');
+                count = 1;
+            });
+        }
+    });
+}
