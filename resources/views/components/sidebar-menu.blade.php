a1d8a99651587d10d6e3d66e0c4237f04fb4adfa
DESKTOP-AEEU8VD\Users <luizrjunior@gmail.com>
2023-01-17 07:52:30 -0300

    Adicionando permissões

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -24,24 +24,30 @@
                 <p>ACL<i class="fas fa-angle-left right"></i></p>
             </a>
             <ul class="nav nav-treeview">
+                @can('Manter_Permissoes')
                 <li class="nav-item">
                     <a id="itemMenuPermissoes" href="{{ url('/acl/permissoes') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Permissões</p>
                     </a>
                 </li>
+                @endcan
+                @can('Manter_Grupos')
                 <li class="nav-item">
                     <a id="itemMenuGrupos" href="{{ url('/acl/grupos') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Grupos de Usuários</p>
                     </a>
                 </li>
+                @endcan
+                @can('Manter_Congregacoes')
                 <li class="nav-item">
                     <a id="itemMenuCongregacoes" href="{{ url('/acl/congregacoes') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Congregações</p>
                     </a>
                 </li>
+                @endcan
             </ul>
         </li>
         @endcan
@@ -52,18 +58,22 @@
                 <p>Financeiro<i class="fas fa-angle-left right"></i></p>
             </a>
             <ul class="nav nav-treeview">
+                @can('Manter_Categorias')
                 <li class="nav-item">
                     <a id="itemMenuCategoriasLancamentos" href="{{ url('/financeiro/categorias-lancamentos') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Categorias</p>
                     </a>
                 </li>
+                @endcan
+                @can('Manter_Lancamentos')
                 <li class="nav-item">
                     <a id="itemMenuLancamentos" href="{{ url('/financeiro/lancamentos') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Lançamentos</p>
                     </a>
                 </li>
+                @endcan
             </ul>
         </li>
         @endcan
