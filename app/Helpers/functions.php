1a8093f2a19068b8488b1a6403a93a9a32d34072
DESKTOP-AEEU8VD\Users <luizrjunior@gmail.com>
2023-01-17 07:51:09 -0300

    Adicionando a função para formatar numero e apresentar no input de valor.

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -17,3 +17,29 @@ if (!function_exists('retornaValorAntigo')) {
     }
 }
 
+if (!function_exists('numberFormatFinaBen')) {
+
+    /**
+     * Formatar Numero AppMatic
+     * @param type $valor
+     * @return type
+     */
+    function numberFormatFinaBen($valor)
+    {
+        if (strstr($valor, '.')) {
+            $valor = number_format($valor, 2, ",", ".");
+            $n = explode(',', $valor);
+            if (count($n) > 1) {
+                if (strlen($n[1]) == 1) {
+                    $valor = $n[0] . "," . $n[1] . "0";
+                }
+            }
+        } else {
+            $valor = number_format($valor, 2, ",", ".");
+        }
+        return $valor;
+    }
+}
+
+
+
