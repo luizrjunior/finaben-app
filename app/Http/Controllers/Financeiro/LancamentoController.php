0d37b83023ac0eebf0afb4123b845c33a78e74ed
DESKTOP-AEEU8VD\Users <luizrjunior@gmail.com>
2023-02-05 13:06:52 -0300

    Correção de erro;

--- app/Http/Controllers/Financeiro/LancamentoController.php
+++ app/Http/Controllers/Financeiro/LancamentoController.php
@@ -39,8 +39,8 @@ class LancamentoController extends Controller
         $session_congregacao_id = Session::get('session_congregacao_id');
         $session_congregacao_uf = Session::get('session_congregacao_uf');
         $session_disabled = "disabled";
-
-        if (auth()->user()->hasAnyRoles('Administrator_Master', 'Administrador_Geral', 'Tesoureiro_Geral', 'Conselho_Fiscal', 'Bispo_Geral')) {
+//, 'Administrador_Geral', 'Tesoureiro_Geral', 'Conselho_Fiscal', 'Bispo_Geral'
+        if (auth()->user()->hasAnyRoles('Administrator_Master') || auth()->user()->hasAnyRoles('Administrador_Geral') ||auth()->user()->hasAnyRoles('Tesoureiro_Geral') ||auth()->user()->hasAnyRoles('Conselho_Fiscal') ||auth()->user()->hasAnyRoles('Bispo_Geral')) {
             $session_congregacao_id = null;
             $uf_session = null;
             $session_disabled = "";
